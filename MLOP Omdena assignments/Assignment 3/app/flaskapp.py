1018f7f7a6fa39f01f57bf7c0a9848b1b523bda6
Syed Muhammad Masab Hshmi <sm2hashmi@outlook.com>
2022-01-07 18:30:08 +0500

    another changes

---
+++ MLOP Omdena assignments/Assignment 3/app/flaskapp.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from BMICalculator import calculate_bmi
+
+app = Flask('__main__')
+
+'''
+A function to get the request using flask and return 
+the calculated bmi.
+'''
+@app.route('/', methods=['GET', 'POST'])
+def get_input():
+
+    packet = request.get_json(force=True)
+    weight = packet['weight']
+    height = packet['height']
+    bmi = calculate_bmi(weight, height)
+    return jsonify(bmi) 
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ MLOP Omdena assignments/Assignment 3/wsgi.py
@@ -0,0 +1,2 @@
+from app.flaskapp import app
+from app.BMICalculator import BMI_calculator 
